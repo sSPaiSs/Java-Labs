4ba979e21c29bbe53713453bca8d18ff3935d911
sSPaiSs <naumenkoroma27@gmail.com>
2023-09-29 23:09:16 +0300

    Update Lab2.java

--- Lab2/Lab2.java
+++ Lab2/Lab2.java
@@ -1,1 +1,13 @@
-wefwerewr
+public class Lab2{
+    public static double Tusk_22(duble a,double b,double c,double d) throws Exception
+    {
+        double result = 0;
+
+        double firstPart = 4 * Math.sinh(Math.pow(Math.abs(a/b), 1.0/2));
+        double secondPart = 3 * Math.asin(c);
+
+        result = Math.pow(firstPart + secondPart, d);
+
+        retuen result;
+    }
+}
